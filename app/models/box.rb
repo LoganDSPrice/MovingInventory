9c60e96f38e5c81c990696ee7ad3bf8def0feb78
Logan Price <logandsprice@gmail.com>
2019-07-25 13:26:07 -0500

    Clean up forms

--- app/models/box.rb
+++ app/models/box.rb
@@ -2,4 +2,8 @@ class Box < ApplicationRecord
   belongs_to :destination, required: false
   belongs_to :house
   has_many :items, dependent: :nullify
+
+  def option_display_value
+    "#{id} - #{description}"
+  end
 end
